886ff4a0ed9d279801d393cac7b5a1ad2157719c
emmogba <emmogba@gmail.com>
2023-02-15 15:07:05 +0530

    third commit

--- config/env/production.js
+++ config/env/production.js
@@ -148,9 +148,9 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     cors: {
-       allowOrigins: [
-        'http://localhost:8081',
-     ]
+      allRoutes: true,
+      allowOrigins: '*',
+      allowCredentials: true,
     },
 
   },
