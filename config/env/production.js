e0ee8e6afcd14472fedfb90dd85b45a02124713d
emmogba <emmogba@gmail.com>
2023-02-16 10:20:03 +0100

    fourth commit

--- config/env/production.js
+++ config/env/production.js
@@ -147,11 +147,6 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    cors: {
-      allRoutes: true,
-      allowOrigins: '*',
-      allowCredentials: true,
-    },
 
   },
 
